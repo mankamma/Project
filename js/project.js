eb6e10d00fc75f8a45f433b9728ab04f1eac59f0
carrie703 <n01603699@humber.ca>
2023-12-08 20:52:44 -0500

    js

--- js/project.js
+++ js/project.js
@@ -1,32 +1,45 @@
 function validateText(){
 
-    let date = document.forms["resForm1"]["setTodaysDate"].value;
-    let time = document.forms["resForm1"]["time"].value;
-    let name = document.forms["resForm1"]["name"].value;
-    let phone = document.forms["resForm1"]["phone"].value;
+    let date=document.forms["resForm1"]["setTodaysDate"].value;
+    let time=document.forms["resForm1"]["time"].value;
+    let name=document.forms["resForm1"]["name"].value;
+    let phone=document.forms["resForm1"]["phone"].value;
+    let guest=document.forms["resForm1"]["guests"].value;
 
-    if (date ==="" || time ==="" || name ==="" || date ==="" || phone ===""){
-        document.getElementById("error").innerHTML = "Please complete the form.";
+    let errorMessage="";
+
+    if (date ===""){
+        errorMessage += "Please select a date.<br>";
     }
 
     if (time === "time-select"){
-        document.getElementById("error").style.color = "red";
-        document.getElementById("error").style.fontSize = "larger";
-        document.getElementById("error").innerHTML = "Please select a time";
+        errorMessage += "Please select a time.<br>";
+
     }
 
     if (name === ""){
-        document.getElementById("error").style.color = "red";
-        document.getElementById("error").style.fontSize = "larger";
-        document.getElementById("error").innerHTML = "Please fill in your full name.";
+        errorMessage += "Please fill in your full name.<br>";
+
     }
 
     if (phone === ""){
-        document.getElementById("error").style.color = "red";
-        document.getElementById("error").style.fontSize = "larger";
-        document.getElementById("error").innerHTML = "Please fill in your phone number.";
+        errorMessage += "Please fill in a phone number.<br>";
+
+    }
+
+    if (guest === ""){
+        errorMessage += "Please select the numbers of guest.<br>";
+
     }
     
+    // Check if there are any error messages
+    if (errorMessage !== "") {
+        document.getElementById("error").style.fontSize = "10px";
+        document.getElementById("error").style.color = "red";
+        document.getElementById("error").innerHTML = errorMessage;
+        return false; // Prevent form submission
+    }
+
     if (date !=="" && time !=="" && name !=="" && date !=="" && phone !==""){
     alert("Your request has been received. We will contact you for confirmation.");
         window.location.href = "confirmation_page.html";
