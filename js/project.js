373572cdc15aae106f5bea6ece04e62cd122c93b
carrie703 <n01603699@humber.ca>
2023-12-08 19:42:02 -0500

    js

--- js/project.js
+++ js/project.js
@@ -5,27 +5,30 @@ function validateText(){
     let name = document.forms["resForm1"]["name"].value;
     let phone = document.forms["resForm1"]["phone"].value;
 
-    if ( date === ""){
-        document.getElementById("error").innerHTML = "Please select a date.";
+    if (date ==="" || time ==="" || name ==="" || date ==="" || phone ===""){
+        document.getElementById("error").innerHTML = "Please complete the form.";
     }
 
-    else if (time === "time-select"){
+    if (time === "time-select"){
         document.getElementById("error").style.color = "red";
         document.getElementById("error").style.fontSize = "larger";
         document.getElementById("error").innerHTML = "Please select a time";
     }
 
-    else if (name === ""){
+    if (name === ""){
         document.getElementById("error").style.color = "red";
         document.getElementById("error").style.fontSize = "larger";
         document.getElementById("error").innerHTML = "Please fill in your full name.";
     }
 
-    else if (phone === ""){
+    if (phone === ""){
         document.getElementById("error").style.color = "red";
         document.getElementById("error").style.fontSize = "larger";
         document.getElementById("error").innerHTML = "Please fill in your phone number.";
     }
+    
+    if (date !=="" && time !=="" && name !=="" && date !=="" && phone !==""){
     alert("Your request has been received. We will contact you for confirmation.");
         window.location.href = "confirmation_page.html";
+    }    
 }
